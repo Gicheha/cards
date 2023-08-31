1b5ec33370cf46ad9727e5dc7c735812abfd590c
Gicheha <geoffreygicheha@gmail.com>
2023-08-31 03:32:01 +0300

    bug fixes and optimizations

--- src/main/java/com/restful/service/cardsservice/service/CardsService.java
+++ src/main/java/com/restful/service/cardsservice/service/CardsService.java
@@ -11,10 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
